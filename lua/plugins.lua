367ea356d9f7e0696381e17ed7798eea4186f21d
vcfhe <61342945+heewq@users.noreply.github.com>
2022-12-01 23:33:01 +0800

    remove 'mg979/vim-visual-multi'

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,7 +2,7 @@ return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use 'windwp/nvim-autopairs'
-  use 'mg979/vim-visual-multi'
+  --use 'mg979/vim-visual-multi'
   use 'editorconfig/editorconfig-vim'
   --use 'neovim/nvim-lspconfig'
   use 'tpope/vim-surround'
